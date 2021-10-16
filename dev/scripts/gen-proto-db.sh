3378d16a181431a558a81ed312cc666c420e24c6
Joseph Subash Kanichai <kjosephsubash@gmail.com>
2021-10-16 17:57:10 +0530

    refactor for proto.go

--- dev/scripts/gen-proto-db.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-OUT=.
-IN=user-service/grpc-user-service/user-service.proto
-mkdir -p $OUT
-protoc --go_out=$OUT --go_opt=paths=source_relative --go-grpc_out=$OUT --go-grpc_opt=paths=source_relative $IN
