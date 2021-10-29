d15276fd48f12139154ea0916c28fe2dff05edfe
Joseph Subash Kanichai <kjosephsubash@gmail.com>
2021-10-29 13:49:25 +0530

    createUser, addUserToScope

--- user-db-service/src/main.go
+++ user-db-service/src/main.go
@@ -5,19 +5,28 @@ import (
 	"github.com/open-exam/open-exam-backend/shared"
 	pb "github.com/open-exam/open-exam-backend/user-db-service/grpc-user-db-service"
 	"google.golang.org/grpc"
+	"os"
+	"strconv"
 )
 
 var (
 	db *sql.DB
 	mode = "prod"
+	standardPasswordSize uint32
 )
 
 func main() {
-
 	shared.SetEnv(&mode)
+
+	validateOptions()
 
 	shared.DefaultGrpcServer(db, func(server *grpc.Server) {
 		s, _ := NewServer()
 		pb.RegisterUserServiceServer(server, s)
 	})
+}
+
+func validateOptions() {
+	tempSize, _ := strconv.ParseUint(os.Getenv("standard_password_size"), 10, 32)
+	standardPasswordSize = uint32(tempSize)
 }
--- user-db-service/src/service.go
+++ user-db-service/src/service.go
@@ -2,9 +2,14 @@ package main
 
 import (
 	"context"
+	"database/sql"
 	_ "database/sql"
+	"encoding/hex"
 	"errors"
+	"github.com/go-sql-driver/mysql"
+	sharedPb "github.com/open-exam/open-exam-backend/grpc-shared"
 	pb "github.com/open-exam/open-exam-backend/user-db-service/grpc-user-db-service"
+	"github.com/open-exam/open-exam-backend/util"
 )
 
 type Server struct {
@@ -32,43 +37,84 @@ func (s *Server) FindOne(ctx context.Context, req *pb.FindOneRequest) (*pb.User,
 	return nil, errors.New("id or email not given")
 }
 
-func getUser(mode int, data string, getPass bool) (*pb.User, error) {
-	user := &pb.User{}
-
-	query := "SELECT id, email, type"
-
-	if getPass {
-		query += ", password "
+func (s *Server) CreateUser(ctx context.Context, req *pb.User) (*pb.User, error) {
+	if len(req.Email) == 0 {
+		return nil, errors.New("email is required")
 	}
-	query += "FROM users WHERE "
 
-	if mode == 0 {
-		query += "id"
+	password := generatePassword(standardPasswordSize)
+	passHash, err := generateFromPassword(password)
+	if err != nil {
+		return nil, errors.New("unknown error while generating password")
+	}
+
+	Id := hex.EncodeToString(util.GenerateRandomBytes(32))
+
+	_, err = db.Exec("INSERT INTO users VALUES (?, ?, ?, ?, ?)", Id, req.Email, req.Type, passHash, req.Name)
+	if err != nil {
+		if err.(*mysql.MySQLError).Number == 1062 {
+			rows := db.QueryRow("SELECT id FROM users WHERE email=?", req.Email)
+
+			err = rows.Scan(&Id)
+			if err != nil {
+				return nil, err
+			}
+
+			return &pb.User {
+				Id: Id,
+			}, nil
+		}
+
+		return nil, err
+	}
+
+	return &pb.User {
+		Id: Id,
+	}, nil
+}
+
+func (s *Server) AddUserToScope(ctx context.Context, req *pb.AddUser) (*sharedPb.StandardStatusResponse, error) {
+	if len(req.Id) == 0 {
+		return nil, errors.New("id is required")
+	}
+
+	if req.Scope == 0 || req.ScopeType == 0 {
+		return nil, errors.New("scope and scopeType are required")
+	}
+
+	row := db.QueryRow("SELECT type FROM users WHERE id=?", req.Id)
+
+	var userType uint32
+	err := row.Scan(&userType)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return &sharedPb.StandardStatusResponse{
+				Status: false,
+				Message: "user does not exist",
+			}, nil
+		}
+
+		return nil, err
+	}
+
+	if userType == 1 && req.ScopeType != 4 {
+		return &sharedPb.StandardStatusResponse{
+			Status: false,
+			Message: "student can only be assigned to teams",
+		}, nil
+	}
+
+	if userType == 1 {
+		_, err = db.Exec("INSERT INTO students(id, team_id) VALUES (?,?)", req.Id, req.Scope)
 	} else {
-		query += "email"
+		_, err = db.Exec("INSERT INTO standard_users VALUES (?,?,?)", req.Id, req.Scope, req.ScopeType)
 	}
-	query += "=?"
 
-	rows, err := db.Query(query, data)
 	if err != nil {
 		return nil, err
 	}
-	defer rows.Close()
 
-	for rows.Next() {
-		if getPass {
-			if err := rows.Scan(&user.Id, &user.Email, &user.Type, &user.Password); err != nil {
-				return nil, err
-			}
-		} else {
-			if err := rows.Scan(&user.Id, &user.Email, &user.Type); err != nil {
-				return nil, err
-			}
-		}
-	}
-
-	if err := rows.Err(); err != nil {
-		return nil, err
-	}
-	return user, nil
+	return &sharedPb.StandardStatusResponse {
+		Status: true,
+	}, nil
 }
