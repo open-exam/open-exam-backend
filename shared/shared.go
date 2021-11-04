33550d24bde89a7f92bd5ae4910239167ec9fa1d
Joseph Subash Kanichai <kjosephsubash@gmail.com>
2021-11-04 19:47:18 +0530

    nil object error fix, RBACMiddleware, plugin shared structs

--- shared/shared.go
+++ shared/shared.go
@@ -1,22 +1,29 @@
 package shared
 
 import (
+	"context"
 	"crypto/rsa"
 	"database/sql"
 	"errors"
 	"flag"
-	"github.com/gin-gonic/gin"
-	"github.com/go-sql-driver/mysql"
-	"github.com/golang-jwt/jwt"
-	"github.com/joho/godotenv"
-	"google.golang.org/grpc"
-	"google.golang.org/grpc/health"
-	healthpb "google.golang.org/grpc/health/grpc_health_v1"
+	"io"
 	"log"
 	"net"
+	"net/http"
 	"os"
 	"strings"
 	"time"
+
+	"github.com/gin-gonic/gin"
+	"github.com/go-git/go-git/v5"
+	"github.com/go-sql-driver/mysql"
+	"github.com/golang-jwt/jwt"
+	"github.com/joho/godotenv"
+	"github.com/mholt/archiver/v3"
+	rbacPb "github.com/open-exam/open-exam-backend/rbac-service/grpc-rbac-service"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/health"
+	healthpb "google.golang.org/grpc/health/grpc_health_v1"
 )
 
 type GinErrorList struct {
@@ -47,6 +54,7 @@ var (
 		ServiceConnection: errors.New("could not connect to service"),
 		UnknownError: errors.New("an unknown error occurred"),
 	}
+	Db *sql.DB
 )
 
 func SetEnv(mode *string) {
@@ -69,18 +77,19 @@ func SetEnv(mode *string) {
 	}
 }
 
-func DefaultGrpcServer (db *sql.DB, registerComponents func(*grpc.Server)) {
+func DefaultGrpcServer(registerComponents func(*grpc.Server)) {
 	var err error
-
+	
 	var (
 		dbUser = os.Getenv("db_user")
 		dbPasswd = os.Getenv("db_pass")
 		dbHost = os.Getenv("db_host")
 		dbPort = os.Getenv("db_port")
 		listenAddr = os.Getenv("listen_addr")
+
 	)
 
-	log.Println("user-db-service starting...")
+	log.Println("service starting...")
 
 	dbAddr := net.JoinHostPort(dbHost, dbPort)
 	dbConfig := mysql.Config{
@@ -92,11 +101,11 @@ func DefaultGrpcServer (db *sql.DB, registerComponents func(*grpc.Server)) {
 	}
 
 	for {
-		db, err = sql.Open("mysql", dbConfig.FormatDSN())
+		Db, err = sql.Open("mysql", dbConfig.FormatDSN())
 		if err != nil {
 			goto dbError
 		}
-		err = db.Ping()
+		err = Db.Ping()
 		if err != nil {
 			goto dbError
 		}
@@ -106,7 +115,7 @@ func DefaultGrpcServer (db *sql.DB, registerComponents func(*grpc.Server)) {
 		log.Println("error connecting to the database, retrying in 5 secs...")
 		time.Sleep(5 * time.Second)
 	}
-
+	
 	lis, err := net.Listen("tcp", listenAddr)
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
@@ -136,6 +145,7 @@ func GetGrpcConn(connectionString string) (*grpc.ClientConn, error) {
 func JwtMiddleware(publicKey *rsa.PublicKey, mode string) gin.HandlerFunc {
 	return func (ctx *gin.Context) {
 		if mode == "dev" {
+			ctx.Set("user", UserJWT{UserId: "1", Scope: 34535})
 			ctx.Next()
 			return
 		}
@@ -176,4 +186,85 @@ func JwtMiddleware(publicKey *rsa.PublicKey, mode string) gin.HandlerFunc {
 		ctx.Set("data", claims["data"])
 		ctx.Next()
 	}
+}
+
+func RBACMiddleware(rbacService string, resource string, operations []string) gin.HandlerFunc {
+	conn, err := GetGrpcConn(rbacService)
+	if err != nil {
+		log.Fatalf("could not connect to rbac service: %v", err)
+	}
+
+	rbacClient := rbacPb.NewRbacServiceClient(conn)
+
+	return func (ctx *gin.Context) {
+		user, exists := ctx.Get("user")
+		if !exists {
+			ctx.AbortWithStatus(401)
+			return
+		}
+
+		res, err := rbacClient.CanPerformOperation(context.Background(), &rbacPb.CanPerformOperationRequest{
+			UserId: user.(UserJWT).UserId,
+			Resource: resource,
+			Operation: operations,
+			Scope: user.(UserJWT).Scope,
+		})
+
+		if err != nil {
+			ctx.AbortWithStatusJSON(500, err)
+			return
+		}
+
+		if !res.Status {
+			ctx.AbortWithStatus(403)
+			return
+		}
+
+		ctx.Next()
+	}
+}
+
+func GetPluginSources(Id string, name string, uri string, uriType string) (int, error) {
+	switch uriType {
+		case "git": {
+			_, err := git.PlainClone(name, false, &git.CloneOptions{
+				URL: uri,
+			})
+			if err != nil {
+				return 400, errors.New("could not clone repository: " + err.Error())
+			}
+		}
+		case "file": {
+			resp, err := http.Get(uri)
+			if err != nil {
+				return 400, errors.New("could not download plugin: " + err.Error())
+			}
+			defer resp.Body.Close()
+	
+			if resp.StatusCode != 200 {
+				return 400, errors.New("could not download plugin: " + resp.Status)
+			}
+	
+			out, err := os.Create(name + "/" + Id)
+			if err != nil {
+				return 500, Errors.UnknownError
+			}
+			defer out.Close()
+	
+			_, err = io.Copy(out, resp.Body)
+			if err != nil {
+				return 500, Errors.UnknownError
+			}
+	
+			err = archiver.Unarchive(name + "/" + Id, name)
+			if err != nil {
+				return 400, errors.New("could not unarchive plugin: " + err.Error())
+			}
+		}
+		default: {
+			return 400, errors.New("invalid uri_type: " + uriType)
+		}
+	}
+
+	return 200, nil
 }
