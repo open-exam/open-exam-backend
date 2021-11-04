1550b8fd9ac8edfa0c6a188a637cfab8d6de2537
Joseph Subash Kanichai <kjosephsubash@gmail.com>
2021-11-04 19:49:36 +0530

    public-api addPlugin

--- public-api/src/plugins.go
+++ public-api/src/plugins.go
@@ -1,12 +1,15 @@
 package main
 
 import (
+	"context"
 	"encoding/hex"
 	"io/ioutil"
-	"log"
 	"os"
 
 	"github.com/gin-gonic/gin"
+	containerPb "github.com/open-exam/open-exam-backend/container-build-service/grpc-container-build-service"
+	sharedPb "github.com/open-exam/open-exam-backend/grpc-shared"
+	pluginDbPb "github.com/open-exam/open-exam-backend/plugin-db-service/grpc-plugin-db-service"
 	"github.com/open-exam/open-exam-backend/shared"
 	"github.com/open-exam/open-exam-backend/util"
 )
@@ -14,6 +17,7 @@ import (
 type Plugin struct {
 	Name string `json:"name" binding:"required"`
 	Uri string `json:"uri" binding:"required"`
+	UriType string `json:"uri_type" binding:"required"`
 	Version string `json:"version" binding:"required"`
 	Organization uint64 `json:"organization" binding:"required"`
 }
@@ -21,14 +25,14 @@ type Plugin struct {
 func InitPlugins(router *gin.RouterGroup) {
 	router.Use(shared.JwtMiddleware(jwtPublicKey, mode))
 
-	router.POST("/", addPlugin)
+	router.POST("/", shared.RBACMiddleware(rbacService, "PLUGINS", []string{"CREATE"}), addPlugin)
 	router.GET("/", getPlugins)
 }
 
 func addPlugin(ctx *gin.Context) {
 	var plugin Plugin
 	if err := ctx.BindJSON(&plugin); err != nil {
-		ctx.AbortWithStatusJSON(400, shared.GinErrors.JsonParseError)
+		ctx.AbortWithStatusJSON(400, err)
 		return
 	}
 
@@ -39,12 +43,62 @@ func addPlugin(ctx *gin.Context) {
 		return
 	}
 
-	log.Println(name)
-	
+	resp, err := shared.GetPluginSources(Id, name, plugin.Uri, plugin.UriType)
+	if err != nil {
+		ctx.AbortWithStatusJSON(resp, gin.H {
+			"error": err.Error(),
+		})
+		return
+	}
 
-	defer func() {
-		os.RemoveAll(name)
-	}()
+	if _, err := shared.ParsePlugin(name); err != nil {
+		ctx.AbortWithStatusJSON(400, gin.H {
+			"error": "could not validate plugin: " + err.Error(),
+		})
+		return
+	}
+
+	conn, err := shared.GetGrpcConn(pluginDbService)
+	if err != nil {
+		ctx.AbortWithStatusJSON(500, shared.GinErrors.ServiceConnection)
+		return
+	}
+
+	client := pluginDbPb.NewPluginServiceClient(conn)
+
+	res, err := client.AddPlugin(context.Background(), &pluginDbPb.Plugin {
+		Name: plugin.Name,
+		Uri: plugin.Uri,
+		UriType: plugin.UriType,
+		Version: plugin.Version,
+		Organization: plugin.Organization,
+	})
+	if err != nil {
+		ctx.AbortWithStatusJSON(500, shared.GinErrors.UnknownError)
+		return
+	}
+
+	containerClient := containerPb.NewContainerClient(conn)
+	resBuild, err := containerClient.Build(context.Background(), &sharedPb.StandardIdRequest {
+		IdInt: res.IdInt,
+	})
+	if err != nil {
+		ctx.AbortWithStatusJSON(500, err)
+		return
+	}
+
+	if !resBuild.Status {
+		ctx.AbortWithStatusJSON(400, gin.H {
+			"error": resBuild.Message,
+		})
+		return
+	}
+
+	ctx.JSON(200, gin.H {
+		"id": res.IdInt,
+	})
+
+	defer os.RemoveAll(name)
 }
 
 func getPlugins(ctx *gin.Context) {
